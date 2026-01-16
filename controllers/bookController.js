6c55037c4ae6feed11dc39b0e4aaa2bc4b53706a
Sreelakshmi-MP2004 <sreelakshmisivadas04@gmail.com>
2026-01-16 16:09:09 +0530

    url changed

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -152,8 +152,8 @@ exports.buyBook = async (req, res) => {
         ];
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ['card'],
-            success_url: 'http://localhost:5173/payment-success',
-            cancel_url: 'http://localhost:5173/payment-error',
+            success_url: 'https://bookstore-frontend-design-only-pi.vercel.app/payment-success',
+            cancel_url: 'https://bookstore-frontend-design-only-pi.vercel.app/payment-error',
             line_items,
             mode: 'payment',
         });
