63d34b4febef545e7f838649d3f23c952340f054
Sreelakshmi-MP2004 <sreelakshmisivadas04@gmail.com>
2026-01-16 16:24:53 +0530

    frontend url change

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -152,8 +152,8 @@ exports.buyBook = async (req, res) => {
         ];
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ['card'],
-            success_url: 'https://bookstore-frontend-design-only-pi.vercel.app/payment-success',
-            cancel_url: 'https://bookstore-frontend-design-only-pi.vercel.app/payment-error',
+            success_url: 'https://bookstore-frontend-design-only-xwnu.vercel.app/payment-success',
+            cancel_url: 'https://bookstore-frontend-design-only-xwnu.vercel.app/payment-error',
             line_items,
             mode: 'payment',
         });
